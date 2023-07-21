84ad195ccbbfaac81f9f0ffb701de40315771dbe
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-07-20 17:59:02 -0700

    install react router and set up root path

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import App from "./routes/App-copy"
 import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import Root from "./routes/root"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
 
 const el = document.getElementById("root")
 const root = ReactDOM.createRoot(el)
 
-root.render(<App />)
+root.render(<RouterProvider router={router} />)
