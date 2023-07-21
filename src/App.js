6c7c717eafa4a45d47f0a6287022f476cb701b88
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-07-20 17:29:08 -0700

    add mobile header and navigation skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { useState } from "react"
+
 function App() {
-  return <div>App</div>
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div>
+      <div className="flex justify-between p-4 border-b-2">
+        <h1 className="text-2xl">Sage and Oak</h1>
+        <button onClick={handleClick}>Open drawer</button>
+      </div>
+      {isOpen && (
+        <div className="px-4">
+          <div className="py-2 border-b-2">HOME</div>
+          <div className="py-2 border-b-2">LOCATION</div>
+          <div className="py-2 border-b-2">PHOTOS</div>
+          <div className="py-2 border-b-2">AVAILABILITY</div>
+          <div className="py-2 border-b-2">REVIEWS</div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App
