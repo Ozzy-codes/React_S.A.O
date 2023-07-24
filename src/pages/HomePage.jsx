835c5914f13136974088579c882cc4279a422eb7
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-07-24 13:53:50 -0700

    add rotating photo timer function on mounted component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from "react"
+
 export default function HomePage() {
+  useEffect(() => {
+    let timer
+    const rotateTimer = () => {
+      console.log("display first photo")
+      timer = setInterval(() => {
+        console.log("Rotate new photo in")
+      }, 3000)
+    }
+    rotateTimer()
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
   return <div>Home Page</div>
 }
