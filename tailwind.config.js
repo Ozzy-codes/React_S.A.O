0ec4def542f9fb9c71c37f8b6a7d0496d77710c4
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-08-24 08:09:22 -0700

    add custom transitionDown keyframes and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        translateDown: {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" }
+        }
+      },
+      animation: {
+        translateDown: "translateDown .5s ease-out"
+      }
+    }
   },
   plugins: []
 }
