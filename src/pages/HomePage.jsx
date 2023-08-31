f62a60289ee6e93f446dbc8e2e532d3f2c10ffc3
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-01 04:02:23 +0800

    hide overflow in the X axis

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,7 +21,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col">
       <img
-        className="w-full"
+        className="w-screen"
         src="https://picsum.photos/200"
         alt="random img"
       />
@@ -29,11 +29,11 @@ export default function HomePage() {
         Luxury Cabin With Breath Taking Views
       </h2>
       <p className="m-4">
-        Welcome to Sage and Oak Cabin, your very own secluded oasis with
-        breathtaking views! This beautiful, custom-built 3000 sq foot cabin is
-        nestled atop its own hill in the peaceful side of the Smokies,
-        surrounded by the woods, giving you all of the privacy you will need for
-        a relaxing retreat.
+        Welcome to Sage and Oak Cabin, your very own secluded oasis
+        with breathtaking views! This beautiful, custom-built 3000 sq
+        foot cabin is nestled atop its own hill in the peaceful side
+        of the Smokies, surrounded by the woods, giving you all of the
+        privacy you will need for a relaxing retreat.
       </p>
       <Button
         soft_corners
@@ -42,11 +42,11 @@ export default function HomePage() {
       </Button>
       <FeatureAccordion />
       <p className="m-4">
-        Blending rustic and modern elements, you will surely feel that you have
-        escaped from your everyday life. Clean and crisp sheets, cozy throw
-        blankets, fresh mountain air, a relaxing hot tub, and carefully curated
-        decor will stimulate all of your senses. Most importantly, enjoy the
-        spectacular view!
+        Blending rustic and modern elements, you will surely feel that
+        you have escaped from your everyday life. Clean and crisp
+        sheets, cozy throw blankets, fresh mountain air, a relaxing
+        hot tub, and carefully curated decor will stimulate all of
+        your senses. Most importantly, enjoy the spectacular view!
       </p>
     </div>
   )
