290ceb0c1f43dcd850b8479f1ee6b370ec7c5bfb
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-08-31 13:03:56 +0800

    rm useEffect, and add bg animation on top level div

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -6,38 +6,38 @@ import Button from "./Button"
 
 function MobileHeader({ header, navLinks }) {
   const [isOpen, setIsOpen] = useState(false)
-  const [previousPageHeight, setPreviousPageHeight] = useState(0)
-  const hiddenTimer = useRef()
+  // const [previousPageHeight, setPreviousPageHeight] = useState(0)
+  // const hiddenTimer = useRef()
 
   const handleClick = () => {
     setIsOpen(!isOpen)
   }
 
-  useEffect(() => {
-    const handler = (event) => {
-      const top_header_div = document.querySelector("#top_header_div")
-      const scrollHeight = event.target.scrollingElement.scrollTop
+  // useEffect(() => {
+  //   const handler = (event) => {
+  //     const top_header_div = document.querySelector("#top_header_div")
+  //     const scrollHeight = event.target.scrollingElement.scrollTop
 
-      if (scrollHeight - previousPageHeight > 0) {
-        if (!top_header_div.classList.contains("opacity-0")) {
-          hiddenTimer.current = setTimeout(() => {
-            top_header_div.classList.add("hidden")
-          }, 1000)
-        }
-        top_header_div.classList.add("opacity-0")
-      } else {
-        clearTimeout(hiddenTimer.current)
-        top_header_div.classList.remove("hidden")
-        setTimeout(() => {
-          top_header_div.classList.remove("opacity-0")
-        }, 200)
-      }
-      setPreviousPageHeight(scrollHeight)
-    }
-    document.addEventListener("scroll", handler)
+  //     if (scrollHeight - previousPageHeight > 0) {
+  //       if (!top_header_div.classList.contains("opacity-0")) {
+  //         hiddenTimer.current = setTimeout(() => {
+  //           top_header_div.classList.add("hidden")
+  //         }, 1000)
+  //       }
+  //       top_header_div.classList.add("opacity-0")
+  //     } else {
+  //       clearTimeout(hiddenTimer.current)
+  //       top_header_div.classList.remove("hidden")
+  //       setTimeout(() => {
+  //         top_header_div.classList.remove("opacity-0")
+  //       }, 200)
+  //     }
+  //     setPreviousPageHeight(scrollHeight)
+  //   }
+  //   document.addEventListener("scroll", handler)
 
-    return () => document.removeEventListener("scroll", handler)
-  }, [previousPageHeight])
+  //   return () => document.removeEventListener("scroll", handler)
+  // }, [previousPageHeight])
 
   const default_Nav_styles =
     "my-4 py-2 border-b-2 hover:underline active:decoration-sky-500 active:text-sky-500 text-2xl"
@@ -83,12 +83,12 @@ function MobileHeader({ header, navLinks }) {
 
       <div
         className={
-          "w-screen relative transition-all duration-1000 " +
+          "w-screen h-screen absolute transition-all duration-1000 " +
           (isOpen ? "left-0 opacity-100" : "left-full opacity-0")
         }>
         <div
           id="drawer_div"
-          className="px-4 h-full bg-gray-100 flex flex-col justify-center text-center transition duration-1000 ">
+          className="px-4 h-full pt-[4.25rem] bg-gray-100 flex flex-col justify-center text-center transition duration-1000 ">
           {renderedLinks}
           <Button
             soft_corners
