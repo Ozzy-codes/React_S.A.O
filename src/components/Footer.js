82a9d4356977e4bc421ae4cb30a5e701aa4eea7b
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-04 11:31:27 +0800

    add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <p>&copy; 2023 SAGE + OAK CABIN &#xB7; (865) 484-6066 &#xB7;</p>
+      <p>SAGEANDOAKCABIN@GMAIL.COM</p>
+      <p>FACEBOOK &#xB7; INSTAGRAM</p>
+    </div>
+  )
+}
