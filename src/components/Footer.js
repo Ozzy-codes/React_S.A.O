c5687332b495b198eec10a4f8390d4391e364fd9
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-04 15:15:32 +0800

    add brand img to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ import { SiInstagram } from "react-icons/si"
 export default function Footer() {
   return (
     <div className="list_spacing text-center my-5 mx-1 text-lg">
+      <img
+        src="https://uc.orez.io/f/6b76dd8c2b214f1db488cbc33ffd8eff"
+        alt="Sage & Oak"
+        className="px-20"
+      />
       <p>
         &copy; 2023 SAGE + OAK CABIN &#xB7;{" "}
         <a
