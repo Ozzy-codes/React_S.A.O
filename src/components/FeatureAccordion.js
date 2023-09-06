4e641f78a0067fa8b8bec63556f6008aff56ed11
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-06 11:56:30 +0800

    add a single useRef hook to hold an array of elements
    Individual references are passed into the click handler, and maxHeight
    styling can be updated.
    Requires f/u: the transition time is currently inflexible, large volumes
    of drawer children produces too fast a transition. Consider a check of
    scroll height and readjustment of transition time.

--- src/components/FeatureAccordion.js
+++ src/components/FeatureAccordion.js
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { GoChevronDown, GoChevronLeft } from "react-icons/go"
 import { items } from "../seeds/AccordionData"
 
 function FeatureAccordion() {
   const [expandedIndex, setExpandedIndex] = useState(-1)
+  const subjectRef = useRef([])
 
-  const handleClick = (nextIndex) => {
+  const handleClick = (nextIndex, reference) => {
+    const drawer = reference.nextSibling
+    if (drawer.style.maxHeight !== "0px") {
+      drawer.style.maxHeight = "0px"
+    } else {
+      drawer.style.maxHeight = drawer.scrollHeight + "px"
+    }
+
     setExpandedIndex((currentExpandedIndex) => {
       if (currentExpandedIndex === nextIndex) {
         return -1
@@ -47,8 +55,11 @@ function FeatureAccordion() {
     return (
       <div key={item.id}>
         <div
+          ref={(element) => (subjectRef.current[index] = element)}
           className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
-          onClick={() => handleClick(index)}>
+          onClick={() =>
+            handleClick(index, subjectRef.current[index])
+          }>
           {item.label}
           {icon}
         </div>
