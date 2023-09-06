9d0083af9f9dc828a9721e8fb7cce9985b0a7f40
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-07 07:49:42 +0800

    remove locationPage link on nav menu

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import { Outlet } from "react-router-dom"
 import Footer from "./components/Footer"
 import MobileHeader from "./components/MobileHeader"
 
-const pages = [
-  "Home",
-  "Location",
-  "Photos",
-  "Availability",
-  "Reviews"
-]
+const pages = ["Home", "Photos", "Availability", "Reviews"]
 const header = "Sage and Oak"
 
 function App() {
