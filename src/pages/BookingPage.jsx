86de95b56b795df4db5f836808feeb5c6f7f6bc3
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-08 06:58:18 +0800

    ATTEMPT AT EMBEDING BOOKING WIDGET - not complete
    Problem lies in the height of the widget not resizing as the window
    grow. This typically happens after you pick dates and the number of guests
    that will stay. The quote automatically appears and the content of the
    window shifts down and out of the top level view.
    I've considered that we could use the postMessage function to communicate
    between the widget window and the parent but, it dones't seem viable
    considering I don't have control over what messages the widget can send.

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,3 +1,36 @@
-export default function BookingPage() {
-  return <div>BookingPage</div>
+export default function bookingpage() {
+  // const data_widget_id = "afd5ae158721478f8a231d1f1bf4124d"
+  // const data_property_id = "ad27789d5c644113b84e38dea08436de"
+
+  const iframe = document.querySelector(".ownerrez-widget-iframe")
+  console.log("iframe object", iframe)
+
+  return (
+    <div>
+      <img
+        className="w-full"
+        src="https://picsum.photos/200"
+        alt="random img"
+      />
+      <div className="p-4">
+        <iframe
+          src="https://secure.ownerreservations.com/widgets/afd5ae158721478f8a231d1f1bf4124d?seq=0&amp;propertykey=ad27789d5c644113b84e38dea08436de&amp;referrer=http%3a%2f%2flocalhost%3a3000%2fbooking#http%3a%2f%2flocalhost%3a3000%2fbooking"
+          seamless="seamless"
+          allowtransparency="true"
+          className="ownerrez-widget-iframe"
+          title="booking/inquiry widget"
+          style={{
+            width: "100%",
+            border: "0px",
+            overflow: "hidden",
+            objectFit: "contain",
+            height: "518px"
+          }}></iframe>{" "}
+        <script src="https://secure.ownerreservations.com/widget.js"></script>
+      </div>
+    </div>
+  )
+}
+{
+  /* <iframe src="https://secure.ownerreservations.com/widgets/afd5ae158721478f8a231d1f1bf4124d?seq=0&amp;propertykey=ad27789d5c644113b84e38dea08436de&amp;referrer=http%3a%2f%2flocalhost%3a3000%2fbooking#http%3a%2f%2flocalhost%3a3000%2fbooking" frameborder="0" scrolling="no" seamless="seamless" allowtransparency="true" class="ownerrez-widget-iframe" title="booking/inquiry widget" style="width: 100%; border: 0px; overflow: hidden; height: 518px;"></iframe> */
 }
