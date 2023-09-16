273167522fcbba868100a7f8ac6cceee2aaaf868
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-16 09:15:55 +0800

    initialize AvailabilityPage

--- src/pages/AvailabilityPage.jsx
+++ src/pages/AvailabilityPage.jsx
@@ -1,3 +1,21 @@
+// import "../components/widgetScript"
+import { useEffect } from "react"
+
 export default function AvailabilityPage() {
-  return <div>Availability Page</div>
+  useEffect(() => {
+    // window.OwnerRez.loadDefaultWidgets()
+  })
+
+  return (
+    <div>
+      <img
+        className="w-full"
+        src="https://picsum.photos/200"
+        alt="random img"
+      />
+      <div className="p-4">
+        <h2 className="text-3xl py-4 mb-4">Availability</h2>
+      </div>
+    </div>
+  )
 }
