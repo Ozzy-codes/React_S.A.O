c2caabc7ca98311534a315988018cc49d3729f58
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-16 10:09:27 +0800

    add single calendar availability OR widget

--- src/pages/AvailabilityPage.jsx
+++ src/pages/AvailabilityPage.jsx
@@ -1,9 +1,9 @@
-// import "../components/widgetScript"
+import "../components/widgetScript"
 import { useEffect } from "react"
 
 export default function AvailabilityPage() {
   useEffect(() => {
-    // window.OwnerRez.loadDefaultWidgets()
+    window.OwnerRez.loadDefaultWidgets()
   })
 
   return (
@@ -15,6 +15,11 @@ export default function AvailabilityPage() {
       />
       <div className="p-4">
         <h2 className="text-3xl py-4 mb-4">Availability</h2>
+        <div
+          className="ownerrez-widget"
+          data-property-id="ad27789d5c644113b84e38dea08436de"
+          data-widget-type="Multiple Month Calendar"
+          data-widget-id="cc1543b3f17d4a48bfbe78eeb50ead8b"></div>
       </div>
     </div>
   )
