bb578832419a10a1c3d40e2ecd4864c7bf351398
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-17 16:47:06 +0800

    initialize photoPage

--- src/pages/PhotoPage.jsx
+++ src/pages/PhotoPage.jsx
@@ -1,3 +1,14 @@
 export default function PhotoPage() {
-  return <div>Photo Page</div>
+  return (
+    <div>
+      <img
+        className="w-full"
+        src="https://picsum.photos/200"
+        alt="random img"
+      />
+      <div className="p-4">
+        <h2 className="text-3xl py-4 mb-4">Gallery</h2>
+      </div>
+    </div>
+  )
 }
