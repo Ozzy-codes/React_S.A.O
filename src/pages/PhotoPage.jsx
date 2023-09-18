8f9407c0dd333055dd74c5c95e80dfb30a4df8ab
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-18 14:10:14 +0800

    insert flex columns within a flex row

--- src/pages/PhotoPage.jsx
+++ src/pages/PhotoPage.jsx
@@ -1,4 +1,14 @@
 export default function PhotoPage() {
+  const imgList1 = []
+  const imgList2 = []
+  for (let index = 0; index < 11; index++) {
+    imgList1.push(
+      <div className="bg-sky-400 border-2 w-auto">item {index}</div>
+    )
+    imgList2.push(
+      <div className="bg-sky-400 border-2 w-auto">item {index}</div>
+    )
+  }
   return (
     <div>
       <img
@@ -8,6 +18,14 @@ export default function PhotoPage() {
       />
       <div className="p-4">
         <h2 className="text-3xl py-4 mb-4">Gallery</h2>
+        <div className="flex justify-between">
+          <div className="gallery_items flex flex-col w-full ">
+            {imgList1}
+          </div>
+          <div className="gallery_items flex flex-col w-full ">
+            {imgList2}
+          </div>
+        </div>
       </div>
     </div>
   )
