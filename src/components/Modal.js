9f750d854b39b857e2a54548116f25978d6b0cda
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-21 12:10:54 +0800

    add fixed buttons and react icons

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom"
 import { imgArray } from "./PhotoLists"
+import { FaCircleChevronLeft } from "react-icons/fa6"
+import { FaCircleChevronRight } from "react-icons/fa6"
 import { useEffect } from "react"
 
 function Modal({ onClose, imgIndex, nextImg, previousImg }) {
@@ -16,14 +18,22 @@ function Modal({ onClose, imgIndex, nextImg, previousImg }) {
       <div
         onClick={onClose}
         className="z-20 fixed inset-0 bg-gray-300 opacity-80 cursor-zoom-out"></div>
+      <button
+        className="z-40 text-4xl text-gray-500 fixed left-[.5rem] top-1/2 -translate-y-2/4 py-[1rem] pr-[1rem]"
+        onClick={previousImg}>
+        <FaCircleChevronLeft />
+      </button>
       <div className="z-30 fixed inset-2/4 -translate-x-2/4 -translate-y-2/4 bg-white w-fit h-fit max-w-[95%] max-h-[85%]">
         {/* //TODO must disable buttons when the length of the array is
           // reached */}
-          <button onClick={previousImg}>Previous</button>
           {imgArray[imgIndex]}
-          <button onClick={nextImg}>Next</button>
         </div>
       </div>
+      <button
+        className="z-40 text-4xl text-gray-500 fixed right-[.5rem] top-1/2 -translate-y-2/4 py-[1rem] pl-[1rem]"
+        onClick={nextImg}>
+        <FaCircleChevronRight />
+      </button>
     </div>,
     document.querySelector(".modal-container")
   )
