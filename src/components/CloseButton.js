587a821396fd66627e4e450fe9b1726e5e1b4d57
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-09-21 21:31:21 +0800

    finalize CloseButton component

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -1,12 +1,14 @@
-export default function CloseButton() {
+export default function CloseButton({ onClose }) {
   const topDivStyle = "fixed right-0 h-6 w-9 -translate-y-full"
 
-  const baseStyle = "w-10 h-2 block bg-gray-500 fixed"
+  const baseStyle = "w-8 h-1 block bg-gray-300 fixed"
   const rotateTopSpan = " top-0 rotate-[135deg]"
   const rotateBottomSpan = " top-0 -rotate-[135deg]"
 
   return (
-    <button className={topDivStyle}>
+    <button
+      onClick={onClose}
+      className={topDivStyle}>
       <span className={baseStyle + rotateTopSpan}>&nbsp;</span>
       <span className={baseStyle + rotateBottomSpan}>&nbsp;</span>
     </button>
