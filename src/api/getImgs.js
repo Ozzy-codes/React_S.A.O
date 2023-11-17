f0d0062dd047ece099d4caa8af252533b3281561
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-11-16 20:49:09 -0800

    Filter axios response data

--- src/api/getImgs.js
+++ src/api/getImgs.js
@@ -3,6 +3,10 @@ import axios from "axios"
 export const getImgs = async () => {
   return await axios
     .get("https://picsum.photos/v2/list")
-    .then((results) => results.data)
+    .then((results) =>
+      results.data.map((element) => {
+        return element.download_url
+      })
+    )
     .catch((error) => console.error(error))
 }
