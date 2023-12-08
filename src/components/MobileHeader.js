d07f5a24050c4380ce07120286a06a0bd781430e
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-12-07 18:45:31 -0800

    fix sliding menu bug when hitting banner
    Menu of links was not moving out of view when user clicks
    banner in top left corner. Added an event handler to
    update state if set to true.

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -11,6 +11,12 @@ function MobileHeader({ header, navLinks }) {
     document.body.classList.toggle("overflow-hidden")
     setIsOpen(!isOpen)
   }
+  const handleBannerClick = () => {
+    if (isOpen) {
+      document.body.classList.remove("overflow-hidden")
+      setIsOpen(!isOpen)
+    }
+  }
 
   const default_Nav_styles =
     "my-4 py-2 hover:underline active:decoration-sky-500 active:text-sky-500 text-4xl"
@@ -46,7 +52,9 @@ function MobileHeader({ header, navLinks }) {
         "absolute w-screen flex flex-col transition-all duration-1000 "
       }>
       <div className="flex z-10 justify-between p-8">
-        <Link to={"/"}>
+        <Link
+          to={"/"}
+          onClick={handleBannerClick}>
           <h1 className="text-4xl">
             <img
               src="https://uc.orez.io/f/6b76dd8c2b214f1db488cbc33ffd8eff"
