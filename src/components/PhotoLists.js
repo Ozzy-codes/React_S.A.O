5221c6592a906c00733c81bdcef82c48f29b574e
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-12-14 15:20:43 -0800

    add imgLoader to Gallery list
    TODO:
    - need to fix key bug. LoadingContent don't have unique keys.

--- src/components/PhotoLists.js
+++ src/components/PhotoLists.js
@@ -1,4 +1,8 @@
+import useTopImgLoader from "../hooks/use-TopImgLoader"
+
 export default function PhotoLists({ findImgIndex, listOfImgs }) {
+  const { isLoaded, setLoadTrue, loadingContent } = useTopImgLoader()
+
   const imgArray = listOfImgs
   const imgList1 = []
   const imgList2 = []
@@ -11,21 +15,35 @@ export default function PhotoLists({ findImgIndex, listOfImgs }) {
   for (let index = 0; index < imgArray.length; index++) {
     if (index % 2 === 0) {
       imgList1.push(
-        <img
-          onClick={() => handleClick(index)}
-          key={index}
-          src={imgArray[index]}
-          alt="picsum test img"
-        />
+        <div
+          className={
+            "relative " + (isLoaded ? "" : "h-[20vh] w-full")
+          }>
+          <img
+            onClick={() => handleClick(index)}
+            key={index}
+            src={imgArray[index]}
+            alt="picsum test img"
+            onLoad={setLoadTrue}
+          />
+          {loadingContent}
+        </div>
       )
     } else {
       imgList2.push(
-        <img
-          onClick={() => handleClick(index)}
-          key={index}
-          src={imgArray[index]}
-          alt="picsum test img"
-        />
+        <div
+          className={
+            "relative " + (isLoaded ? "" : "h-[20vh] w-full")
+          }>
+          <img
+            onClick={() => handleClick(index)}
+            key={index}
+            src={imgArray[index]}
+            alt="picsum test img"
+            onLoad={setLoadTrue}
+          />
+          {loadingContent}
+        </div>
       )
     }
   }
