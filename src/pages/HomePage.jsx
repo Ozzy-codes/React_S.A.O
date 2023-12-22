ad05bd6e6d41760d206b797ef089285496a1cd5a
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-12-22 14:46:32 -0800

    create GoogleMap cmpnt

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import FeatureAccordion from "../components/FeatureAccordion"
 import Button from "../components/Button"
 import TopImg from "../components/TopImg"
-import useLoading from "../hooks/use-Loading"
+import GoogleMap from "../components/GoogleMap"
 import { Link } from "react-router-dom"
 import { useEffect } from "react"
 
 export default function HomePage() {
-  const { isLoaded, setLoadTrue, widgetLoadingContent } = useLoading()
-
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -56,25 +54,7 @@ export default function HomePage() {
         </p>
       </div>
       <h2 className="text-3xl m-4">Map</h2>
-      <div className="p-4 bg-white">
-        <div
-          className={
-            "relative " + (isLoaded ? "" : "h-[90vh] w-full")
-          }>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.274167456993!2d-83.6046403246361!3d35.74406547256694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885eab89d3aaece9%3A0xc272ce6478e3f4dc!2sSage%20%2B%20Oak%20Cabin!5e0!3m2!1sen!2sid!4v1694040788912!5m2!1sen!2sid"
-            title="map"
-            width="600"
-            height="450"
-            style={{ border: "0" }}
-            allowFullScreen=""
-            loading="lazy"
-            className="w-full"
-            referrerPolicy="no-referrer-when-downgrade"
-            onLoad={setLoadTrue}></iframe>
-          {widgetLoadingContent}
-        </div>
-      </div>
+      <GoogleMap />
     </div>
   )
 }
