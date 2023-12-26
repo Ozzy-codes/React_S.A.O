3d25000a9a4af2b959438e1c692067abeb482c87
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-12-26 10:04:00 -0800

    add 3 month available calendar widget

--- src/components/CabinAvailability.js
+++ src/components/CabinAvailability.js
@@ -1,8 +1,10 @@
 import Skeleton from "../components/Skeleton"
 import useLoading from "../hooks/use-Loading"
+import { useState } from "react"
 
 function CabinAvailability({ className }) {
   const { isLoaded, loadingRef } = useLoading()
+  const [screenMd, setScreenMd] = useState(false)
 
   const widgetLoadingContent = isLoaded ? (
     ""
@@ -13,17 +15,34 @@ function CabinAvailability({ className }) {
     />
   )
 
+  const widgetOption = screenMd ? (
+    <div
+      className="ownerrez-widget"
+      data-property-id="ad27789d5c644113b84e38dea08436de"
+      data-widget-type="Display 3 months - Multiple Month Calendar"
+      data-widget-id="feafd37a5298462c88492b3d62cef241"
+      ref={loadingRef}></div>
+  ) : (
+    <div
+      className="ownerrez-widget"
+      data-property-id="ad27789d5c644113b84e38dea08436de"
+      data-widget-type="Multiple Month Calendar"
+      data-widget-id="cc1543b3f17d4a48bfbe78eeb50ead8b"
+      ref={loadingRef}></div>
+  )
+
   return (
     <div
       className={
         "relative " + (isLoaded ? "" : "h-[90vh] w-full " + className)
       }>
-      <div
+      {/* <div
         className="ownerrez-widget"
         data-property-id="ad27789d5c644113b84e38dea08436de"
         data-widget-type="Multiple Month Calendar"
         data-widget-id="cc1543b3f17d4a48bfbe78eeb50ead8b"
-        ref={loadingRef}></div>
+        ref={loadingRef}></div> */}
+      {widgetOption}
       {widgetLoadingContent}
     </div>
   )
