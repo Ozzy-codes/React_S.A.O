d93f6a7c75ad1410be885bc0169cc0f0fbd0bdb1
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-12-26 18:53:16 -0800

    create specific widget components

---
+++ src/components/SingleMonthView.js
@@ -0,0 +1,31 @@
+import useLoading from "../hooks/use-Loading"
+import Skeleton from "../components/Skeleton"
+
+function SingleMonthView({ className }) {
+  const { isLoaded, loadingRef } = useLoading()
+
+  const widgetLoadingContent = isLoaded ? (
+    ""
+  ) : (
+    <Skeleton
+      times={1}
+      className={"absolute inset-0"}
+    />
+  )
+
+  return (
+    <div
+      className={
+        "relative " + (isLoaded ? "" : "h-[90vh] w-full " + className)
+      }>
+      <div
+        className="ownerrez-widget "
+        data-property-id="ad27789d5c644113b84e38dea08436de"
+        data-widget-type="Multiple Month Calendar"
+        data-widget-id="cc1543b3f17d4a48bfbe78eeb50ead8b"
+        ref={loadingRef}></div>
+      {widgetLoadingContent}
+    </div>
+  )
+}
+export default SingleMonthView
