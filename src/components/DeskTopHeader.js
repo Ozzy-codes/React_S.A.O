3542a5f31ba5cf24e3165bbea0c244a64ded93a1
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-01-01 15:13:33 -0800

    update DeskTopHeader link style

--- src/components/DeskTopHeader.js
+++ src/components/DeskTopHeader.js
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom"
+import Button from "./Button"
 
 function DeskTopHeader({ header, navLinks }) {
   const default_Nav_styles =
-    "my-4 py-2 md:my-2 md:py-1 md:text-[5vh] hover:underline active:decoration-sky-500 active:text-sky-500 text-4xl"
+    "my-2 py-1 text-[2rem] hover:underline active:decoration-sky-500 active:text-sky-500 "
 
   const renderedLinks = navLinks.map((page, index) => {
     if (index === 0) {
@@ -29,12 +30,10 @@ function DeskTopHeader({ header, navLinks }) {
   return (
     <div
       id="top_header_div"
-      className={
-        "absolute w-screen flex flex-col items-center transition-all duration-1000 "
-      }>
-      <div className="flex z-20 justify-between p-8 md:pt-[4rem] md:pb-0 md:px-[4rem]">
+      className={"absolute w-screen flex flex-col items-center "}>
+      <div className="z-20 pt-[4rem] pb-0 px-[4rem]">
         <Link to={"/"}>
-          <h1 className="text-6xl md:text-[5rem] font-['Whisper'] text-black">
+          <h1 className="leading-none text-[5rem] font-['Whisper'] text-black">
             Sage + Oak
             {/* <img
               src="https://uc.orez.io/f/6b76dd8c2b214f1db488cbc33ffd8eff"
@@ -42,6 +41,20 @@ function DeskTopHeader({ header, navLinks }) {
               className="w-20 rounded-lg"
             /> */}
           </h1>
+        </Link>
+      </div>
+      <div
+        id="drawer_div"
+        className="z-20 w-full bg-transparent flex justify-evenly text-center ">
+        {renderedLinks}
+        <Link
+          to={"booking"}
+          className="">
+          <Button
+            soft_corners
+            className="w-full leading-[2.25rem] text-[2rem] p-4 bg-transparent border-0 underline text-[darkGreen] ">
+            Book Now
+          </Button>
         </Link>
       </div>
     </div>
