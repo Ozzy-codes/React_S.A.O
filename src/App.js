87475019e62b188ece7f2a1e3b485af0bf5c0a8c
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-01-01 10:08:34 -0800

    import DeskTopHeader and resize hook into App cmpnt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./components/Footer"
 import MobileHeader from "./components/MobileHeader"
+import DeskTopHeader from "./components/DeskTopHeader"
+import useResize from "./hooks/use-Resize"
 
 const pages = ["Home", "Photos", "Availability", "Reviews"]
 const header = "Sage and Oak"
 
 function App() {
+  const { nextViewPort } = useResize(1024)
+
+  const headerOption = nextViewPort ? (
+    <DeskTopHeader
+      header={header}
+      navLinks={pages}
+    />
+  ) : (
+    <MobileHeader
+      header={header}
+      navLinks={pages}
+    />
+  )
+
   return (
     <div>
-      <MobileHeader
-        header={header}
-        navLinks={pages}
-      />
+      {headerOption}
       <div>
         <Outlet />
       </div>
