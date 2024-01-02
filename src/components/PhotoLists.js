9f83a6cf76632e92f42fd12694406aae84485782
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-01-01 17:57:34 -0800

    create PhotoList cmpnt views controlled by window size

--- src/components/PhotoLists.js
+++ src/components/PhotoLists.js
@@ -1,74 +1,23 @@
-import useLoading from "../hooks/use-Loading"
-import Skeleton from "./Skeleton"
+import TwoColView from "../components/TwoColView"
+import FourColView from "./FourColView"
+import useResize from "../hooks/use-Resize"
 
 export default function PhotoLists({ findImgIndex, listOfImgs }) {
-  const { isLoaded, setLoadTrue } = useLoading()
+  const { nextViewPort } = useResize(1024)
 
-  const loadingContent = isLoaded ? (
-    ""
+  const colOption = nextViewPort ? (
+    <FourColView
+      findImgIndex={findImgIndex}
+      listOfImgs={listOfImgs}
+    />
   ) : (
-    <Skeleton
-      times={1}
-      className={"absolute inset-0"}
+    <TwoColView
+      findImgIndex={findImgIndex}
+      listOfImgs={listOfImgs}
     />
   )
 
-  const imgArray = listOfImgs
-  const imgList1 = []
-  const imgList2 = []
-
-  const handleClick = (index) => {
-    console.log("photoLists cmpnt is sending index up")
-    findImgIndex(index)
-  }
-
-  for (let index = 0; index < imgArray.length; index++) {
-    const { imgWidth, imgHeight, imgUrl } = imgArray[index]
-    if (index % 2 === 0) {
-      imgList1.push(
-        <div
-          className="relative "
-          key={index}>
-          <img
-            onClick={() => handleClick(index)}
-            src={imgUrl}
-            alt="picsum test img"
-            onLoad={setLoadTrue}
-            width={imgWidth}
-            height={imgHeight}
-            className="w-full"
-          />
-          {loadingContent}
-        </div>
-      )
-    } else {
-      imgList2.push(
-        <div
-          className="relative "
-          key={index}>
-          <img
-            onClick={() => handleClick(index)}
-            src={imgUrl}
-            alt="picsum test img"
-            onLoad={setLoadTrue}
-            width={imgWidth}
-            height={imgHeight}
-            className="w-full"
-          />
-          {loadingContent}
-        </div>
-      )
-    }
-  }
-
   return (
-    <div className="flex gap-1.5 justify-between">
-      <div className="gallery_items flex gap-1.5 flex-col w-full ">
-        {imgList1}
-      </div>
-      <div className="gallery_items flex gap-1.5 flex-col w-full ">
-        {imgList2}
-      </div>
-    </div>
+    <div className="flex gap-1.5 justify-between">{colOption}</div>
   )
 }
---
+++ src/components/TwoColView.js
@@ -0,0 +1,74 @@
+import useLoading from "../hooks/use-Loading"
+import Skeleton from "./Skeleton"
+
+function TwoColView({ findImgIndex, listOfImgs }) {
+  const { isLoaded, setLoadTrue } = useLoading()
+
+  const loadingContent = isLoaded ? (
+    ""
+  ) : (
+    <Skeleton
+      times={1}
+      className={"absolute inset-0"}
+    />
+  )
+
+  const imgArray = listOfImgs
+  const imgList1 = []
+  const imgList2 = []
+
+  const handleClick = (index) => {
+    console.log("photoLists cmpnt is sending index up")
+    findImgIndex(index)
+  }
+
+  for (let index = 0; index < imgArray.length; index++) {
+    const { imgWidth, imgHeight, imgUrl } = imgArray[index]
+    if (index % 2 === 0) {
+      imgList1.push(
+        <div
+          className="relative "
+          key={index}>
+          <img
+            onClick={() => handleClick(index)}
+            src={imgUrl}
+            alt="picsum test img"
+            onLoad={setLoadTrue}
+            width={imgWidth}
+            height={imgHeight}
+            className="w-full"
+          />
+          {loadingContent}
+        </div>
+      )
+    } else {
+      imgList2.push(
+        <div
+          className="relative "
+          key={index}>
+          <img
+            onClick={() => handleClick(index)}
+            src={imgUrl}
+            alt="picsum test img"
+            onLoad={setLoadTrue}
+            width={imgWidth}
+            height={imgHeight}
+            className="w-full"
+          />
+          {loadingContent}
+        </div>
+      )
+    }
+  }
+  return (
+    <>
+      <div className="gallery_items flex gap-1.5 flex-col w-full ">
+        {imgList1}
+      </div>
+      <div className="gallery_items flex gap-1.5 flex-col w-full ">
+        {imgList2}
+      </div>
+    </>
+  )
+}
+export default TwoColView
