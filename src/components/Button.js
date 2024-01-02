6b88f241e4ef8fb3c785feb8f9ba1c4624619292
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-01-02 07:14:08 -0800

    update active button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,7 +10,7 @@ export default function Button({
 }) {
   const classes = ClassNames(
     rest.className,
-    "flex justify-center px-3 py-3 border bg-green-900 text-white",
+    "flex justify-center px-3 py-3 border bg-green-900 text-white active:decoration-sky-500 active:text-sky-500",
     {
       "border-blue-500 bg-blue-500 text-white": primary,
       "rounded-full": rounded,
