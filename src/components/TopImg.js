aadd2e7afa6101a52bfb3bde2ba21a421076ba3d
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-01-02 16:36:42 -0800

    add event listener to scroll img up

--- src/components/TopImg.js
+++ src/components/TopImg.js
@@ -1,8 +1,26 @@
 import useLoading from "../hooks/use-Loading"
 import Skeleton from "../components/Skeleton"
+import { useEffect, useRef } from "react"
 
 function TopImg() {
   const { isLoaded, setLoadTrue } = useLoading()
+  const imgTag = useRef(null)
+
+  useEffect(() => {
+    function handleScroll() {
+      const containerHeight =
+        imgTag.current.parentElement.clientHeight
+      const imgHeight = imgTag.current.height
+      const imgHeightDiff = imgHeight - containerHeight
+      if (window.scrollY <= imgHeightDiff) {
+        imgTag.current.style.transform = `translateY(${
+          -window.scrollY / 2
+        }px)`
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
 
   const loadingContent = isLoaded ? (
     ""
@@ -14,14 +32,15 @@ function TopImg() {
   )
 
   return (
-    <div className={"relative header_gradient  "}>
+    <div className={"relative header_gradient h-[750px] z-[-5] "}>
       <img
-        className="w-full md:h-[100vw] "
+        className="w-full "
         src="https://picsum.photos/200/?blur=1"
         alt="random img"
         width={200}
         height={200}
         onLoad={setLoadTrue}
+        ref={imgTag}
       />
       {loadingContent}
     </div>
