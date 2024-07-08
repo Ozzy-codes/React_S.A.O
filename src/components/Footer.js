c2ceaa12e06f76a6d17ee4476d12d88943c8aacf
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-08 12:37:26 -0700

    tighten Footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,39 +4,40 @@ import logo from '../photos/logo/sage_and_oak_logo.png'
 
 export default function Footer() {
   return (
-    <div className="grid md:grid-cols-2 md:px-[5rem] list_spacing text-center py-5 px-1 text-lg bg-[var(--light-wheat)]">
+    <div className="grid md:grid-cols-2 md:px-[5rem] text-center py-5 px-1 text-lg bg-[var(--light-wheat)]">
       <img
         src={logo}
         alt="Sage & Oak"
         className="w-[50vw] justify-self-center md:w-[20vw] md:row-[1_/_5] rounded-[var(--border-radius)] "
       />
       {
-        /*  TODO: Make text a tighter group*/
       }
-      <p>&copy; 2023 SAGE + OAK CABIN</p>
-      <p>
-        <a
-          href="tel:+18654846066"
-          className="underline">
-          865-484-6066
-        </a>
-      </p>
-      <p>
-        <a
-          href="mailto:sageandoakcabin@gmail.com"
-          className="underline">
-          SAGEANDOAKCABIN@GMAIL.COM
-        </a>
-      </p>
-      <p>
-        <a href="https://www.facebook.com/sageandoakcabin">
-          <SiFacebook className="inline-block text-3xl mx-3" />
-        </a>{" "}
-        &#xB7;{" "}
-        <a href="https://www.instagram.com/sageandoakcabin/">
-          <SiInstagram className="inline-block text-3xl mx-3" />
-        </a>
-      </p>
+      <div className="flex flex-col list_spacing mt-12">
+        <p>&copy; 2023 SAGE + OAK CABIN</p>
+        <p>
+          <a
+            href="tel:+18654846066"
+            className="underline">
+            865-484-6066
+          </a>
+        </p>
+        <p>
+          <a
+            href="mailto:sageandoakcabin@gmail.com"
+            className="underline">
+            SAGEANDOAKCABIN@GMAIL.COM
+          </a>
+        </p>
+        <p>
+          <a href="https://www.facebook.com/sageandoakcabin">
+            <SiFacebook className="inline-block text-3xl mx-3" />
+          </a>{" "}
+          &#xB7;{" "}
+          <a href="https://www.instagram.com/sageandoakcabin/">
+            <SiInstagram className="inline-block text-3xl mx-3" />
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
