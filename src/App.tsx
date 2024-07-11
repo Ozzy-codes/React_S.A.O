74ec9f68e20d1fd1eeae3885fdd739d81d8cd8d7
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-07-11 09:30:36 -0700

    update App.js to .tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom"
-import Footer from "./components/Footer"
-import MobileHeader from "./components/MobileHeader"
-import DeskTopHeader from "./components/DeskTopHeader"
-import useResize from "./hooks/use-Resize"
+import Footer from "./componentsTs/Footer"
+import MobileHeader from "./componentsTs/MobileHeader"
+import DeskTopHeader from "./componentsTs/DeskTopHeader"
+import useResize from "./hooksTs/use-Resize"
 
 const pages = ["Home", "Photos", "Availability", "Reviews"]
 
-function App() {
+const App = () => {
   const { nextViewPort } = useResize(1024)
 
   const headerOption = nextViewPort ? (
